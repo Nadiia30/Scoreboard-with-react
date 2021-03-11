8afcb809d86a4258db85dc3c27d98977a5917719
nadiia <nadiia.oleks@gmail.com>
2021-03-11 14:45:59 +0200

    refactor the Counter event handlers, implement new props in the Player and Counter components

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,33 @@ class App extends Component {
     players: [
       {
         name: "Guil",
+        score:0,
         id: 1
       },
       {
         name: "Treasure",
+        score:0,
         id: 2
       },
       {
         name: "Ashley",
+        score:0,
         id: 3
       },
       {
         name: "James",
+        score:0,
         id: 4
       }
     ]
   };
 
+  handleScoreChange = (delta) => {
+    // this.setState( prevState => ({
+    //   score: prevState.score + 1
+    // }));
+    console.log(delta)
+  }
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
       return {
@@ -46,7 +56,9 @@ class App extends Component {
             name={player.name}
             id={player.id}
             key={player.id.toString()} 
-            removePlayer={this.handleRemovePlayer}           
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}  
+            score={player.score}         
           />
         )}
       </div>
